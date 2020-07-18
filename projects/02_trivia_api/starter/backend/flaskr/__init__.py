6c9c198bac6b92b13507c7b3a23bb92694fb2cb9
dnn004 <dnn004@ucsd.edu>
2020-07-18 03:17:08 -0700

    Add quiz's end for every category

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -14,13 +14,13 @@ def create_app(test_config=None):
     setup_db(app)
     app.secret_key = "THIS IS A SECRET KEY"
     '''
-    @TODO: Set up CORS. Allow '*' for origins.
+    @DONE: Set up CORS. Allow '*' for origins.
     Delete the sample route after completing the TODOs
     '''
     cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     '''
-    @TODO: Use the after_request decorator to set Access-Control-Allow
+    @DONE: Use the after_request decorator to set Access-Control-Allow
     '''
     @app.after_request
     def after_request(response):
@@ -35,7 +35,7 @@ def create_app(test_config=None):
         return response
 
     '''
-    @TODO:
+    @DONE:
     Create an endpoint to handle GET requests
     for all available categories.
     '''
@@ -56,7 +56,7 @@ def create_app(test_config=None):
         except:
             abort(404)
     '''
-    @TODO:
+    @DONE:
     Create an endpoint to handle GET requests for questions,
     including pagination (every 10 questions).
     This endpoint should return a list of questions,
@@ -95,7 +95,7 @@ def create_app(test_config=None):
             abort(404)
 
     '''
-    @TODO:
+    @DONE:
     Create an endpoint to DELETE question using a question ID.
 
     TEST: When you click the trash icon next to a question, the question
@@ -108,12 +108,15 @@ def create_app(test_config=None):
         question = Question.query.get(question_id)
         try:
             question.delete()
-            return jsonify({"success": True})
+            return jsonify({
+                "success": True,
+                "deleted_id": question_id
+            })
         except:
             abort(404)
 
     '''
-    @TODO:
+    @DONE:
     Create an endpoint to POST a new question,
     which will require the question and answer text,
     category, and difficulty score.
@@ -148,7 +151,7 @@ def create_app(test_config=None):
             abort(422)
 
     '''
-    @TODO:
+    @DONE:
     Create a POST endpoint to get questions based on a search term.
     It should return any questions for whom the search term
     is a substring of the question.
@@ -173,7 +176,7 @@ def create_app(test_config=None):
         })
 
     '''
-    @TODO:
+    @DONE:
     Create a GET endpoint to get questions based on category.
 
     TEST: In the "List" tab / main screen, clicking on one of the
@@ -196,7 +199,7 @@ def create_app(test_config=None):
             abort(404)
 
     '''
-    @TODO:
+    @DONE:
     Create a POST endpoint to get questions to play the quiz.
     This endpoint should take category and previous question parameters
     and return a random questions within the given category,
@@ -236,10 +239,13 @@ def create_app(test_config=None):
                 "question": return_question.format()
             })
         else:
-            abort(404)
+            return jsonify({
+                "success": True,
+                "question": None
+            })
 
     '''
-    @TODO:
+    @DONE:
     Create error handlers for all expected errors
     including 404 and 422.
     '''
