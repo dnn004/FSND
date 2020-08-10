5164430af126619292b13e13a472e534cad40d7e
dnn004 <dnn004@ucsd.edu>
2020-08-10 03:52:58 -0700

    Complete endpoints

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,15 +2,198 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
+from models import Movie, Actor, movies_actors, setup_db, db,db_drop_and_create_all
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+# create and configure the app
+app = Flask(__name__)
+setup_db(app)
+migrate = Migrate(app, db)
+CORS(app)
 
-  return app
+app.secret_key = "secretKEY"
+# db_drop_and_create_all()
 
-APP = create_app()
+'''Endpoints:
+GET /actors and /movies
+DELETE /actors/ and /movies/
+POST /actors and /movies and
+PATCH /actors/ and /movies/'''
+
+@app.route("/actors", methods=["GET"])
+def get_actors():
+    try:
+        actors = Actor.query.all()
+        formatted_actors = [
+            actor.format() for actor in actors
+        ]
+        return jsonify({
+            "success": True,
+            "actors": formatted_actors
+        })
+    except:
+        abort(404)
+    
+@app.route("/movies", methods=["GET"])
+def get_movies():
+    try:
+        movies = Movie.query.all()
+        formatted_movies = [
+            movie.format() for movie in movies
+        ]
+        return jsonify({
+            "success": True,
+            "movies": formatted_movies
+        })
+    except:
+        abort(404)
+
+@app.route("/actors/<actor_id>", methods=["DELETE"])
+def delete_actors(actor_id):
+    try:
+        actor = Actor.query.get(actor_id)
+        actor.delete()
+        return jsonify({
+            "success": True,
+            "id_deleted": actor_id
+        })
+    except:
+        abort(404)
+
+@app.route("/movies/<movie_id>", methods=["DELETE"])
+def delete_movies(movie_id):
+    try:
+        movie = Movie.query.get(movie_id)
+        movie.delete()
+        return jsonify({
+            "success": True,
+            "id_deleted": movie_id
+        })
+    except:
+        abort(404)
+
+
+@app.route("/actors", methods=["POST"])
+def post_actors():
+    name = request.json.get("name")
+    age = request.json.get("age")
+    gender = request.json.get("gender")
+    movies = request.json.get("movies")
+
+    if not name or not age:
+        abort(400)
+    try:
+        actor  = Actor(
+            name=name,
+            age=age,
+            gender=gender
+        )
+        if movies is not None:
+            movies = Movie.query.filter(Movie.title.in_(movies)).all()
+            actor.movies = movies
+        actor.insert()
+        return jsonify({
+            "success": True,
+            "actor": actor.format()
+        })
+    except:
+        db.session.rollback()
+        abort(422)
+    finally:
+        db.session.close()
+
+
+@app.route("/movies", methods=["POST"])
+def post_movies():
+    title = request.json.get("title")
+    release_date = request.json.get("release_date")
+    actors = request.json.get("actors")
+
+    if not title or not release_date:
+        abort(400)
+    try:
+        movie = Movie(
+            title=title,
+            release_date=release_date
+        )
+        if actors is not None:
+            actors = Actor.query.filter(Actor.name.in_(actors)).all()
+            movie.actors = actors
+        movie.insert()
+        return jsonify({
+            "success": True,
+            "movie": movie.format()
+        })
+    except:
+        db.session.rollback()
+        abort(422)
+    finally:
+        db.session.close()
+
+@app.route("/actors/<actor_id>", methods=["PATCH"])
+def patch_actors(actor_id):
+    name = request.json.get("name")
+    age = request.json.get("age")
+    gender = request.json.get("gender")
+    movies = request.json.get("movies")
+
+    try:
+        actor = Actor.query.get(actor_id)
+    except:
+        abort(404)
+
+    if not name or not age:
+        abort(400)
+
+    try:
+        actor.name = name
+        actor.age = age
+        actor.gender = gender
+        if movies is not None:
+            movies = Movie.query.filter(Movie.title.in_(movies)).all()
+            actor.movies = movies
+        actor.update()
+        return jsonify({
+            "success": True,
+            "actor": actor.format()
+        })
+    except:
+        db.session.rollback()
+        abort(422)
+    finally:
+        db.session.close()
+
+
+@app.route("/movies/<movie_id>", methods=["PATCH"])
+def patch_movies(movie_id):
+    title = request.json.get("title")
+    release_date = request.json.get("release_date")
+    actors = request.json.get("actors")
+
+    try:
+        movie = Movie.query.get(movie_id)
+    except:
+        abort(404)
+
+    if not title or not release_date:
+        abort(400)
+
+    try:
+        movie.title = title
+        movie.release_date = release_date
+        if actors is not None:
+            actors = Actor.query.filter(Actor.name.in_(actors)).all()
+            movie.actors = actors
+        movie.update()
+        return jsonify({
+            "success": True,
+            "movie": movie.format()
+        })
+    except:
+        db.session.rollback()
+        abort(422)
+    finally:
+        db.session.close()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(debug=True)
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,109 @@
 
+import os
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_name = "cast"
+database_path = "postgres://{}/{}".format(':5433', database_name)
+
+db = SQLAlchemy()
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+movies_actors = db.Table("movies_actors",
+    db.Column("movie_id", db.Integer, db.ForeignKey("movies.id"), primary_key=True),
+    db.Column("actor_id", db.Integer, db.ForeignKey("actors.id"), primary_key=True)
+)
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all() 
+
+class Movie(db.Model):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(String)
+    actors = db.relationship(
+        "Actor",
+        secondary=movies_actors,
+        lazy="subquery",
+        backref=db.backref("movies", lazy=True)
+    )
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+  
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        actors = db.session.query(movies_actors).join(Movie).join(Actor).filter(Movie.id == self.id).with_entities(Actor.name).all()
+        actors_return = []
+        for actor in actors:
+            actors_return.append(*actor)
+        return {
+            "id": self.id,
+            "title": self.title,
+            "release_date": self.release_date,
+            "actors": actors_return
+        }
+
+class Actor(db.Model):
+    __tablename__ = "actors"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+  
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        movies = db.session.query(movies_actors).join(Movie).join(Actor).filter(Actor.id == self.id).with_entities(Movie.title).all()
+        movies_return = []
+        for movie in movies:
+            movies_return.append(*movie)
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender,
+            "movies": movies_return
+        }
+
+
